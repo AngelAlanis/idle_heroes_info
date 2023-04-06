3ff695b9482d64323ebcc6e31f4839d72af0c8a1
ahangel256@gmail.com <ahangel256@gmail.com>
2023-04-06 10:32:20 -0600

    String template for hero attributes

--- app/src/main/java/com/example/idleheroesinfo/adapter/HeroViewHolder.kt
+++ app/src/main/java/com/example/idleheroesinfo/adapter/HeroViewHolder.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.idleheroesinfo.R
 import com.example.idleheroesinfo.model.Hero
 
-class HeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class HeroViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     val heroPicture: ImageView = view.findViewById(R.id.hero_picture)
     val heroName: TextView = view.findViewById(R.id.hero_name)
     val heroShortName: TextView = view.findViewById(R.id.hero_short_name)
@@ -15,9 +15,11 @@ class HeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val heroClass: TextView = view.findViewById(R.id.hero_class)
 
     fun render(heroModel: Hero) {
+        val resources = view.context?.resources
+
         heroName.text = heroModel.name
-        heroShortName.text = heroModel.shortName
-        heroFaction.text = heroModel.faction
-        heroClass.text = heroModel.heroClass
+        heroShortName.text = resources?.getString(R.string.short_name, heroModel.shortName)
+        heroFaction.text = resources?.getString(R.string.faction, heroModel.faction)
+        heroClass.text = resources?.getString(R.string.hero_class, heroModel.heroClass)
     }
 }
