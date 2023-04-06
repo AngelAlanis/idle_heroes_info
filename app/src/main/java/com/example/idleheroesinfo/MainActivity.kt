070d501316b388c3e39ac0bfd7dc538d6a83d1ac
ahangel256@gmail.com <ahangel256@gmail.com>
2023-04-06 10:29:36 -0600

    App working

--- app/src/main/java/com/example/idleheroesinfo/MainActivity.kt
+++ app/src/main/java/com/example/idleheroesinfo/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.idleheroesinfo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.idleheroesinfo.adapter.HeroCardAdapter
+import com.example.idleheroesinfo.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRecyclerView()
     }
 
-    fun initRecyclerView(){
-
+    private fun initRecyclerView() {
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = HeroCardAdapter(Datasource(this).loadHeroes())
     }
 }
