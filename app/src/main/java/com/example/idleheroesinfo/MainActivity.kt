cc244d5d4f9defd25241c5ad8fae00ee23b6dbe6
ahangel256@gmail.com <ahangel256@gmail.com>
2023-04-12 15:07:31 -0600

    NavGraph created

--- app/src/main/java/com/example/idleheroesinfo/MainActivity.kt
+++ app/src/main/java/com/example/idleheroesinfo/MainActivity.kt
@@ -2,22 +2,14 @@ package com.example.idleheroesinfo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.idleheroesinfo.adapter.HeroCardAdapter
-import com.example.idleheroesinfo.data.Datasource
+import com.example.idleheroesinfo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        initRecyclerView()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-    private fun initRecyclerView() {
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = HeroCardAdapter(Datasource(this).loadHeroes())
-    }
 }
