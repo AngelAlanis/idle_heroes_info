9dc03796622ec3f7a96074670496cfff883aa33c
ahangel256@gmail.com <ahangel256@gmail.com>
2023-04-18 11:36:15 -0500

    Changed BiographyItem to not be parcelizable and added missing entry for Halora

--- app/src/main/java/com/example/idleheroesinfo/model/BiographyItem.kt
+++ app/src/main/java/com/example/idleheroesinfo/model/BiographyItem.kt
@@ -1,39 +1,8 @@
 package com.example.idleheroesinfo.model
 
-import android.os.Parcel
-import android.os.Parcelable
-
 data class BiographyItem(
     val id: String,
     val heroName: String,
     val title: String,
     val description: String
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(description)
-        parcel.writeString(heroName)
-        parcel.writeString(id)
-        parcel.writeString(title)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<BiographyItem> {
-        override fun createFromParcel(parcel: Parcel): BiographyItem {
-            return BiographyItem(parcel)
-        }
-
-        override fun newArray(size: Int): Array<BiographyItem?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
