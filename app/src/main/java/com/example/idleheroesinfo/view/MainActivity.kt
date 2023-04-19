e6ac2ef47e7d958851cd03a6cb1dc50a88b81d66
ahangel256@gmail.com <ahangel256@gmail.com>
2023-04-19 11:07:24 -0600

    Bottom navigation menu working.

--- app/src/main/java/com/example/idleheroesinfo/view/MainActivity.kt
+++ app/src/main/java/com/example/idleheroesinfo/view/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.idleheroesinfo.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.idleheroesinfo.R
@@ -21,6 +23,22 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        binding.menuBottom.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_heroes -> changeFragment(HeroListFragment())
+                R.id.menu_artifacts -> changeFragment(ArtifactListFragment())
+            }
+            true
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction
+            .replace(R.id.nav_host_fragment, fragment)
+            .commit()
     }
 
     override fun onSupportNavigateUp(): Boolean {
