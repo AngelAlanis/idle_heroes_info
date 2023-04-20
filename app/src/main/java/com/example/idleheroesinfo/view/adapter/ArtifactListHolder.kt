8db8c1e2329f741283ec1bbaa895a4d91be25925
ahangel256@gmail.com <ahangel256@gmail.com>
2023-04-20 08:12:31 -0600

    Changed bottomNavigation background color.

--- app/src/main/java/com/example/idleheroesinfo/view/adapter/ArtifactListHolder.kt
+++ app/src/main/java/com/example/idleheroesinfo/view/adapter/ArtifactListHolder.kt
@@ -1,6 +1,7 @@
 package com.example.idleheroesinfo.view.adapter
 
 import android.app.AlertDialog
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
@@ -12,24 +13,32 @@ import com.example.idleheroesinfo.model.Artifact
 class ArtifactListHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ArtifactItemBinding.bind(view)
-
+    private lateinit var artifact: Artifact
     fun render(artifactModel: Artifact) {
+        artifact = artifactModel
         binding.artifactImage.setImageResource(artifactModel.imageResourceId)
 
         itemView.setOnClickListener {
-            val builder = AlertDialog.Builder(view.context)
-            val inflater = LayoutInflater.from(view.context)
-            val binding = ArtifactInformationBinding.inflate(inflater)
-
-            binding.artifactName.text = view.context.getString(R.string.splendid_artifact, artifactModel.name)
-            binding.artifactDescription.text = artifactModel.description
-            binding.artifactImage.setImageResource(artifactModel.imageResourceId)
-            binding.artifactStats.text = artifactModel.getStatsToString()
-
-            builder.setView(binding.root)
-            builder.create()
-            builder.show()
-
+            showDialog()
         }
+    }
+
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(view.context)
+        val inflater = LayoutInflater.from(view.context)
+        val binding = ArtifactInformationBinding.inflate(inflater)
+
+        binding.artifactName.text =
+            view.context.getString(R.string.splendid_artifact, artifact.name)
+        binding.artifactDescription.text = artifact.description
+        binding.artifactImage.setImageResource(artifact.imageResourceId)
+        binding.artifactStats.text = artifact.getStatsToString()
+
+        builder.setView(binding.root)
+        val dialog = builder.create()
+        dialog.apply {
+            window?.setBackgroundDrawableResource(R.drawable.artifact_information_background)
+        }
+        dialog.show()
     }
 }
